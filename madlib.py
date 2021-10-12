169370421ef2832ab3b9158e83a2e3a9f46ca687
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-12 17:02:13 +0300

    feat: added the initial script for the function parse_template

--- madlib.py
+++ madlib.py
@@ -1,8 +1,25 @@
-def read_template():
-  return("It was a {Adjective} and {Adjective} {Noun}.")
+import re
 
-def parse_template():
-  pass
+def read_template(pth):
+  print('Hello, good day!\nI want you to build me a video game by entering whatever you like based on the describtions in between the curly brackets.\n**************************************************************************************************************************')
+  try:
+    with open(pth) as f:
+      script = f.read()
+  except FileNotFoundError:
+    print('The file path provided is not correct.')
+
+  return script
+
+def parse_template(text):
+  string = 'It was a {Adjective} and {Adjective} {Noun}.'
+  parts = tuple(map(lambda itm : itm.strip('{}'),re.findall(r'{\w+}', string)))
+  stripped = re.sub(r'{\w+}','{}',string )
+  return (stripped, parts)
+  
 
 def merge():
   pass
+
+if __name__ == "__main__":
+  read_template('assets/dark_and_stormy_night_template.txt')
+  parse_template('It was a {Adjective} and {Adjective} {Noun}.')
