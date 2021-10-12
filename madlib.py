44c077032b656ab44c3e57b23c28b7aae18d9c7a
HamzaAhmad97 <h.radiahmad@gmail.com>
2021-10-12 17:11:24 +0300

    feat: added the initial script for the merge function

--- madlib.py
+++ madlib.py
@@ -1,25 +1,32 @@
 import re
 
-def read_template(pth):
-  print('Hello, good day!\nI want you to build me a video game by entering whatever you like based on the describtions in between the curly brackets.\n**************************************************************************************************************************')
-  try:
-    with open(pth) as f:
-      script = f.read()
-  except FileNotFoundError:
-    print('The file path provided is not correct.')
 
-  return script
+def read_template(pth):
+    print(
+        "Hello, good day!\nI want you to build me a video game by entering whatever you like based on the describtions in between the curly brackets.\n**************************************************************************************************************************"
+    )
+    try:
+        with open(pth) as f:
+            script = f.read()
+    except FileNotFoundError as err:
+        raise err
+
+    return script
+
 
 def parse_template(text):
-  string = 'It was a {Adjective} and {Adjective} {Noun}.'
-  parts = tuple(map(lambda itm : itm.strip('{}'),re.findall(r'{\w+}', string)))
-  stripped = re.sub(r'{\w+}','{}',string )
-  return (stripped, parts)
-  
+    string = "It was a {Adjective} and {Adjective} {Noun}."
+    parts = tuple(map(lambda itm: itm.strip("{}"), re.findall(r"{\w+}", string)))
+    stripped = re.sub(r"{\w+}", "{}", string)
+    return (stripped, parts)
 
-def merge():
-  pass
+
+def merge(text, parts):
+    result = text.format(*parts)
+    return result
+
 
 if __name__ == "__main__":
-  read_template('assets/dark_and_stormy_night_template.txt')
-  parse_template('It was a {Adjective} and {Adjective} {Noun}.')
+    read_template("assets/dark_and_stormy_night_template.txt")
+    parse_template("It was a {Adjective} and {Adjective} {Noun}.")
+    merge("It was a {} and {} {}.", ("dark", "stormy", "night"))
